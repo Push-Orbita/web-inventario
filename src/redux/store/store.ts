a3a57067970545c45892edbd8d93de47a49c34f1
silfarias <silvifarias17@gmail.com>
2025-04-30 14:04:01 -0300

    feat: agregamos función para generar rutas dinámicas

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
-import { thunk } from "redux-thunk";
+// import { thunk } from "redux-thunk";
 import storage from "redux-persist/lib/storage"; // almacenamiento por defecto (localStorage)
 
 import uiSlice from "../slices/uiSlices/uiSlice";
@@ -21,8 +21,10 @@ export const store = configureStore({ // creamos el store
     reducer: {
         ui: persistReducer<ReturnType<typeof uiSlice>>(persistUiConfig, uiSlice),
         auth: persistReducer<ReturnType<typeof authSlice>>(persistAuthConfig, authSlice),
-    }, middleware: (getDefaultMiddleware) => { // agregamos redux-thunk para hacer peticiones asincronas
-        return getDefaultMiddleware().concat(thunk);
+    }, middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware({
+            serializableCheck: false
+        })
     },
 });
 
--- src/router/RouterJs.tsx
+++ src/router/RouterJs.tsx
@@ -1,4 +1,5 @@
-import { Suspense, ComponentType, lazy, LazyExoticComponent,  } from "react";
+import { useAppSelector } from "@hooks/reduxHook";
+import { Suspense, ComponentType, lazy, LazyExoticComponent, } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 // cargamos el componente solamente cuando sea necesario (cuando el usuario accede a /home).
@@ -12,27 +13,70 @@ const componentsMap: ComponentsMap = {
 };
 
 export const RouterJs = () => {
+
+    const { userModulos } = useAppSelector(state => state.auth);
+
+    // función para generar las rutas dinámicas
+    const renderRoutes = (modulos: any) => {
+        return modulos.flatMap((modulo: any) => {
+            if (modulo.items && modulo.items.length > 0) {
+                return renderRoutes(modulo.items); // si el modulo tiene hijos (items o submodulos), los renderizamos recursivamente
+            }
+            const Component = componentsMap[modulo.element];
+            if (!Component) {
+                console.warn(`No se encontró componente: ${modulo.element}`);
+                return [];
+            }
+            return (
+                <Route
+                    key={modulo.path}
+                    path={modulo.path}
+                    element={
+                        <Suspense fallback={<div className="preloader-container">
+                            <div id="preloader5"></div>
+                        </div>
+                        }>
+                            <Component />
+                        </Suspense>
+                    }
+                />
+            );
+        });
+    };
+
     return (
         <div>
             <Routes>
-                <Route 
-                    path="/" 
-                    element={
-                        <Navigate 
-                            to="/home" // si el usuario va a '/' lo redirige automáticamente a '/home'
-                            replace  // hace que no se guarde '/' en el historial del navegador.
-                        />
-                    } />
+                {userModulos && userModulos.length > 0 ? (
+                    renderRoutes(userModulos)
+                ) : (
+                    // ruta por defecto cuando no hay módulos disponibles
+                    <Route path="*" element={<Navigate to="/home" replace />} />
+                )}
+                <Route path="/" element={
+                    <Navigate
+                        to="/home" // si el usuario va a '/' lo redirige automáticamente a '/home'
+                        replace  // hace que no se guarde '/' en el historial del navegador.
+                    />
+                } />
                 <Route path="/home" element={
                     <Suspense fallback={ // mientras el componente carga, mostramos un preloader
                         <div className="preloader-container">
                             <div id="preloader5"></div>
                         </div>
-
                     }>
                         <HomeAdmin />
                     </Suspense>
                 } />
+                {/* <Route path="/configuracion-usuario" element={
+                    <Suspense fallback={
+                        <div className="preloader-container">
+                            <div id="preloader5"></div>
+                        </div>
+                    }>
+                        <ConfiguracionUsuario />
+                    </Suspense>
+                } /> */}
             </Routes>
         </div>
     )
